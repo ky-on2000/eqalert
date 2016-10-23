d0bf4800949b3322d731af477dc6d342eae8d810
kyoko <kyoko>
2016-10-23 17:02:46 +0900

    new

--- test.php
+++ test.php
@@ -1,1 +1,5 @@
-http://api.p2pquake.com/v1/userquake?date=2011/03/11
+<?php
+$html = file_get_contents("http://api.p2pquake.com/v1/userquake?date=2011/03/11", false, NULL);
+$html= mb_convert_encoding($html, "UTF-8", "SJIS");
+echo $html;
+?>
