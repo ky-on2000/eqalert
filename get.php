21411c411b506ca9b118fc08847e618e37ed60de
kyoko <kyoko>
2016-10-23 16:31:44 +0900

    New!

---
+++ get.php
@@ -0,0 +1,52 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+header("Content-Type: text/html; charset=UTF-8");
+require_once(__DIR__.'/cache/phpfastcache.php');
+phpfastcache::setup("storage","auto");
+for($i=4;$i>=0;$i-=1){
+	$data[$i]=get(date("Y/m/d",strtotime("-${i} day")));
+}
+//デバッグ用
+//$data[$i]=get("2014/05/03");
+$alldata=explode("\n",implode('',$data));
+$number=count($alldata);
+for($n=0;$n<$number;$n++){
+	if(strpos($alldata[$n],'QUA')===false||substr($alldata[$n],33,33)!=4){
+		$alldata[$n]='';
+	}
+}
+$alldata=array_values(array_filter($alldata));
+$number=count($alldata);
+for($m=0;$m<$number;$m++){
+	$shingen[]=mb_substr($alldata[$m],29,mb_strpos(mb_substr($alldata[$m],29),'/'));
+}
+$shingenD[]=array_filter(array_count_values($shingen),'filter');
+//デバッグ用
+//print_r($shingenD);
+//print_r($shingen5);
+//print_r($alldata);
+//print_r($shingen);
+function get($day){
+	$url = "http://p2pquake.ddo.jp/p2pquake/api_userquake.pl?date=${day}";
+	try {
+	$cache=phpfastcache();
+	$html= $cache->get($url);
+    if ($html==false) {
+      $html = file_get_contents($url, false, NULL);
+	  $html= mb_convert_encoding($html, "UTF-8", "SJIS");
+      if($html!==false) {
+		$cache->set($url,$html,60);
+      }
+      else {
+		  die("エラー地震情報APIへの接続に失敗しました<br>");
+      }
+	}
+	} catch (Exception $e) {
+		die("ERROR地震情報APIへの接続に失敗しました。<br>"); 
+	}
+	return $html;
+}
+function filter($var){
+    return ($var>=12)? TRUE : FALSE;
+}
+?>
---
+++ run.php
@@ -0,0 +1,31 @@
+<?php
+require __DIR__.'/get.php';
+require __DIR__.'/twitteroauth/autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+$ck="HYxUUsHqmLEJZIlJs7rO8j01e";
+$cs="Xx5xl6gmSDR9bIcrgVtXWgITGdzJpFWPV1o7uu8J79JNHF8Tyh";
+$at="4712110981-dwS1v92kSeBzJPbueIwbkIOYBFdZfcgnjlATDxm";
+$as="oAnyxfWvPgG5QXobSeIa3lxHz54ROYKPUFm5HBeAFVU5Z";
+$o=count($shingenD);
+while($o!==0){
+	$o-=1;
+	foreach($shingenD[$o] as $key=>$value){
+		tweet('【注意】'.$key.'を震源とする地震がこの5日間で'.$value.'回発生しています。念のため地震や火山に注意してください。',$ck,$cs,$at,$as);
+		file_put_contents("date.txt",date("Y/m/d"));
+	}
+}
+if(date("Y/m/d",strtotime("-1 day"))==file_get_contents("date.txt")){
+	tweet(date(Y年m月d日).'現在、群発地震は起きていません。',$ck,$cs,$at,$as);
+	file_put_contents("date.txt",date("Y/m/d"));
+}
+function tweet($message,$ck,$cs,$at,$as){
+try{
+	//config
+	$connection = new TwitterOAuth($ck, $cs, $at, $as);
+	$content = $connection->get("account/verify_credentials");
+	$statues = $connection->post("statuses/update", ["status" =>$message]);
+} catch (Exception $e) {
+	print "ERROR:".$e->getMessage();
+}
+}
+?>
